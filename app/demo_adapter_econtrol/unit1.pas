71dbd608d32bee0330bfa660274893eeb2fdd94e
Alexey T <support@uvviewsoft.com>
2015-06-07 15:35:48 +0300

    demo, lexer choice

--- app/demo_adapter_econtrol/unit1.pas
+++ app/demo_adapter_econtrol/unit1.pas
@@ -17,12 +17,15 @@ type
 
   TfmMain = class(TForm)
     chkWrap: TCheckBox;
+    comboLexer: TComboBox;
     Panel1: TPanel;
     procedure chkWrapChange(Sender: TObject);
+    procedure comboLexerChange(Sender: TObject);
     procedure FormCreate(Sender: TObject);
   private
     { private declarations }
     ed: TATSynEdit;
+    procedure UpdateLexList;
   public
     { public declarations }
   end;
@@ -40,17 +43,34 @@ var
 
 { TfmMain }
 
-procedure TfmMain.FormCreate(Sender: TObject);
+procedure TfmMain.UpdateLexList;
 var
-  fname, fname_lxl: string;
+  i: integer;
+  sl: tstringlist;
+begin
+  sl:= tstringlist.create;
+  try
+    for i:= 0 to manager.AnalyzerCount-1 do
+      sl.Add(manager.Analyzers[i].LexerName);
+    sl.sort;
+    comboLexer.Items.AddStrings(sl);
+  finally
+    sl.free;
+  end;
+end;
+
+procedure TfmMain.FormCreate(Sender: TObject);
 const
   lexername = 'Pascal Script';
+var
+  fname, fname_lxl: string;
 begin
   fname:= ExtractFilePath(Application.ExeName)+'test.pas';
   fname_lxl:= ExtractFilePath(Application.ExeName)+'lexlib.lxl';
 
   manager:= TSyntaxManager.Create(Self);
   manager.LoadFromFile(fname_lxl);
+  UpdateLexList;
 
   ed:= TATSynEdit.Create(Self);
   ed.Font.Name:= 'Consolas';
@@ -61,10 +81,11 @@ begin
   ed.Colors.TextBG:= $e0f0f0;
 
   adapter:= TATSynEdit_Adapter_EControl.Create;
-  adapter.InitLexer(manager, lexername);
   ed.AdapterOfHilite:= adapter;
 
   ed.LoadFromFile(fname);
+  comboLexer.ItemIndex:= comboLexer.Items.IndexOf(lexername);
+  comboLexerChange(nil);
 end;
 
 procedure TfmMain.chkWrapChange(Sender: TObject);
@@ -73,6 +94,12 @@ begin
     ed.OptWrapMode:= cWrapOn
   else
     ed.OptWrapMode:= cWrapOff;
+end;
+
+procedure TfmMain.comboLexerChange(Sender: TObject);
+begin
+  adapter.InitLexer(manager, comboLexer.Text);
+  ed.Update;
 end;
 
 end.
