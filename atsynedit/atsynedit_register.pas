41b2a3c7d29738a0002f95f0b9b28a408d355e07
BkUser <hhhhhhhhnn70@gmail.com>
2015-07-25 12:42:45 +0300

    Icons for ide pallet

--- atsynedit/atsynedit_register.pas
+++ atsynedit/atsynedit_register.pas
@@ -5,19 +5,24 @@ unit ATSynEdit_Register;
 interface
 
 uses
-  Classes, SysUtils, ATSynEdit, ATSynEdit_Edits;
+  Classes, SysUtils, ATSynEdit, ATSynEdit_Edits,
+  LResources;
 
 procedure Register;
 
 implementation
-
-//{$R *.dcr}
 
 { Registration }
 procedure Register;
 begin
   RegisterComponents('Misc', [TATSynEdit, TATEdit, TATComboEdit]);
 end;
+
+initialization
+
+//lrs file must be made by command:
+// ~/lazarus/tools/lazres icons.lrs *.png
+{$I res/ide/icons.lrs}
 
 end.
 
