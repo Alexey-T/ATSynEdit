616e52c491271b445062c55c38bd79c675ff9a9f
Alexey-T <support@uvviewsoft.com>
2015-08-12 01:23:43 +0300

    demo LexProp styles

--- app/demo_econtrol_lexer_lib/formlexerprop.pas
+++ app/demo_econtrol_lexer_lib/formlexerprop.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Graphics, FileUtil, Forms, Controls, StdCtrls,
-  Dialogs, ButtonPanel, ComCtrls,
+  Dialogs, ButtonPanel, ComCtrls, ExtCtrls, ColorBox,
   ecSyntAnal,
   ATSynEdit,
   ATSynEdit_Adapter_EControl;
@@ -15,7 +15,15 @@ type
   { TfmLexerProp }
 
   TfmLexerProp = class(TForm)
+    bApplyStl: TButton;
     ButtonPanel1: TButtonPanel;
+    chkBold: TCheckBox;
+    chkItalic: TCheckBox;
+    chkStrik: TCheckBox;
+    chkUnder: TCheckBox;
+    edColorFont: TColorBox;
+    edColorBG: TColorBox;
+    edStyleType: TComboBox;
     edExt: TEdit;
     edLineCmt: TEdit;
     edName: TEdit;
@@ -25,13 +33,28 @@ type
     Label3: TLabel;
     Label4: TLabel;
     edNotes: TMemo;
-    PageControl1: TPageControl;
-    TabSheet1: TTabSheet;
-    TabSheet2: TTabSheet;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    ListStyles: TListBox;
+    chkBorderT: TPageControl;
+    Panel1: TPanel;
+    TabSheetGen: TTabSheet;
+    TabSheetNotes: TTabSheet;
+    TabSheetStyles: TTabSheet;
+    procedure bApplyStlClick(Sender: TObject);
+    procedure edStyleTypeChange(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
+    procedure ListStylesClick(Sender: TObject);
   private
     { private declarations }
+    FAn: TecSyntAnalyzer;
+    procedure UpdateStl;
+    procedure UpdateStlEn(fmt: TecFormatType);
+    procedure UpdateStlFromList;
+    procedure UpdateStlToList;
   public
     { public declarations }
     Adapter: TATAdapterEControl;
@@ -55,11 +78,100 @@ begin
   edSample.AdapterHilite:= Adapter;
 end;
 
+procedure TfmLexerProp.bApplyStlClick(Sender: TObject);
+begin
+  UpdateStlToList;
+end;
+
+procedure TfmLexerProp.edStyleTypeChange(Sender: TObject);
+begin
+  UpdateStlEn(TecFormatType(edStyleType.ItemIndex));
+end;
+
 procedure TfmLexerProp.FormDestroy(Sender: TObject);
 begin
   edSample.AdapterHilite:= nil;
   FreeAndNil(Adapter);
 end;
+
+procedure TfmLexerProp.ListStylesClick(Sender: TObject);
+begin
+  UpdateStlFromList;
+end;
+
+procedure TfmLexerProp.UpdateStl;
+var
+  i: integer;
+  fmt: TecSyntaxFormat;
+begin
+  ListStyles.Items.Clear;
+  for i:= 0 to FAn.Formats.Count-1 do
+  begin
+    fmt:= FAn.Formats[i];
+    ListStyles.Items.Add(fmt.DisplayName);
+  end;
+
+  if ListStyles.Count>0 then
+    ListStyles.ItemIndex:= 0;
+  UpdateStlFromList;
+end;
+
+
+
+procedure TfmLexerProp.UpdateStlFromList;
+var
+  n: integer;
+  fmt: TecSyntaxFormat;
+begin
+  n:= ListStyles.ItemIndex;
+  if n<0 then exit;
+  fmt:= FAn.Formats[n];
+
+  edStyleType.ItemIndex:= Ord(fmt.FormatType);
+  edColorFont.Selected:= fmt.Font.Color;
+  edColorBG.Selected:= fmt.BgColor;
+
+  chkBold.Checked:= fsBold in fmt.Font.Style;
+  chkItalic.Checked:= fsItalic in fmt.Font.Style;
+  chkUnder.Checked:= fsUnderline in fmt.Font.Style;
+  chkStrik.Checked:= fsStrikeOut in fmt.Font.Style;
+
+  UpdateStlEn(fmt.FormatType);
+end;
+
+procedure TfmLexerProp.UpdateStlEn(fmt: TecFormatType);
+begin
+  edColorFont.Enabled:= fmt in [ftCustomFont, ftFontAttr, ftColor];
+  edColorBG.Enabled:= true;
+
+  chkBold.Enabled:= fmt in [ftCustomFont, ftFontAttr];
+  chkItalic.Enabled:= chkBold.Enabled;
+  chkUnder.Enabled:= chkBold.Enabled;
+  chkStrik.Enabled:= chkBold.Enabled;
+end;
+
+procedure TfmLexerProp.UpdateStlToList;
+var
+  n: integer;
+  fmt: TecSyntaxFormat;
+  fs: TFontStyles;
+begin
+  n:= ListStyles.ItemIndex;
+  if n<0 then exit;
+  fmt:= FAn.Formats[n];
+
+  fmt.FormatType:= TecFormatType(edStyleType.ItemIndex);
+  fmt.Font.Color:= edColorFont.Selected;
+  fmt.BgColor:= edColorBG.Selected;
+
+  fs:= [];
+  if chkBold.Checked then Include(fs, fsBold);
+  if chkItalic.Checked then Include(fs, fsItalic);
+  if chkUnder.Checked then Include(fs, fsUnderline);
+  if chkStrik.Checked then Include(fs, fsStrikeOut);
+  fmt.Font.Style:= fs;
+end;
+
 
 function DoShowDialogLexerProp(an: TecSyntAnalyzer; AFontName: string;
   AFontSize: integer): boolean;
@@ -70,10 +182,12 @@ begin
   if an=nil then exit;
   F:= TfmLexerProp.Create(nil);
   try
+    F.FAn:= an;
     F.edName.Text:= an.LexerName;
     F.edExt.Text:= an.Extentions;
     F.edLineCmt.Text:= an.LineComment;
     F.edNotes.Lines.AddStrings(an.Notes);
+    F.UpdateStl;
 
     F.edSample.Font.Name:= AFontName;
     F.edSample.Font.Size:= AFontSize;
