7ea982995bd848ea7d3f56039cb43c66cfe7a0fe
Alexey-T <support@uvviewsoft.com>
2015-08-15 11:34:49 +0300

    demo test_canvasline

---
+++ app/test_canvasline/unit1.pas
@@ -0,0 +1,54 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  ExtCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Panel1: TPanel;
+    procedure Button1Click(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  c: tcanvas;
+begin
+  c:= panel1.canvas;
+  c.brush.color:= clyellow;
+  c.fillrect(0, 0, 200, 200);
+
+  c.pen.color:= clred;
+  c.line(10,10,30,10);
+  c.line(30,10,50,10);
+  c.line(50,10,80,10);
+
+  c.line(10,10,10,30);
+  c.line(10,30,10,50);
+  c.line(10,50,10,80);
+
+end;
+
+end.
+
