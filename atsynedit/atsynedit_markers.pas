e47f30b5234e7292da5a249738fa388feea4787b
Alexey-T <support@uvviewsoft.com>
2015-11-25 21:50:22 +0300

    Markers work

---
+++ atsynedit/atsynedit_markers.pas
@@ -0,0 +1,98 @@
+unit ATSynEdit_Markers;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+type
+  TATMarkerItem = class
+  public
+    PosX, PosY: integer;
+    CoordX, CoordY: integer; //screen coords
+  end;
+
+type
+  { TATMarkers }
+
+  TATMarkers = class
+  private
+    FList: TList;
+    function GetItem(N: integer): TATMarkerItem;
+  public
+    constructor Create; virtual;
+    destructor Destroy; override;
+    procedure Clear;
+    procedure Delete(N: integer);
+    function Count: integer;
+    function IsIndexValid(N: integer): boolean;
+    property Items[N: integer]: TATMarkerItem read GetItem; default;
+    procedure Add(APosX, APosY: integer);
+  end;
+
+implementation
+
+{ TATMarkers }
+
+constructor TATMarkers.Create;
+begin
+  inherited;
+  FList:= TList.Create;
+end;
+
+destructor TATMarkers.Destroy;
+begin
+  Clear;
+  FreeAndNil(FList);
+  inherited;
+end;
+
+procedure TATMarkers.Clear;
+var
+  i: integer;
+begin
+  for i:= FList.Count-1 downto 0 do
+    Delete(i);
+end;
+
+procedure TATMarkers.Delete(N: integer);
+begin
+  if IsIndexValid(N) then
+  begin
+    TObject(FList[N]).Free;
+    FList.Delete(N);
+  end;
+end;
+
+function TATMarkers.Count: integer;
+begin
+  Result:= FList.Count;
+end;
+
+function TATMarkers.IsIndexValid(N: integer): boolean;
+begin
+  Result:= (N>=0) and (N<FList.Count);
+end;
+
+function TATMarkers.GetItem(N: integer): TATMarkerItem;
+begin
+  if IsIndexValid(N) then
+    Result:= TATMarkerItem(FList[N])
+  else
+    Result:= nil;
+end;
+
+procedure TATMarkers.Add(APosX, APosY: integer);
+var
+  Item: TATMarkerItem;
+begin
+  Item:= TATMarkerItem.Create;
+  Item.PosX:= APosX;
+  Item.PosY:= APosY;
+  FList.Add(Item);
+end;
+
+end.
+
