5035f0578b6a5adcbb0353247400b39e2bca2a0c
Alexey-t <support@uvviewsoft.com>
2018-01-12 17:45:32 +0300

    edit descr of one command

--- atsynedit/atsynedit_keymap_init.pas
+++ atsynedit/atsynedit_keymap_init.pas
@@ -101,7 +101,7 @@ begin
   M.Add(cCommand_TextDeleteSelection, 'selection: delete selected text', [], []);
   M.Add(cCommand_SelectInverted, 'selection: invert selection', [], []);
   M.Add(cCommand_SelectSplitToLines, 'selection: split selection into lines', [], []);
-  M.Add(cCommand_SelectExtendByLine, 'selection: extend selection by line', [cXControl+'+L'], []);
+  M.Add(cCommand_SelectExtendByLine, 'selection: expand selection to line', [cXControl+'+L'], []);
   M.Add(cCommand_SelectWords, 'selection: select words at carets', [], []);
   M.Add(cCommand_SelectLines, 'selection: select lines at carets', [], []);
   M.Add(cCommand_SelectNone, 'selection: cancel selection', [], []);
