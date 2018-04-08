65030e3674d645a6d46aad877fdeddc048c7cde5
Alexey-t <support@uvviewsoft.com>
2018-04-08 12:17:43 +0300

    fix resource script

--- atsynedit/res/make_res.sh
+++ atsynedit/res/make_res.sh
@@ -1,7 +1,6 @@
 ~/lazarus/tools/lazres nicescroll1.res mouse_scroll.cur=atsyn_move mouse_scroll_up.cur=atsyn_move_u mouse_scroll_down.cur=atsyn_move_d mouse_scroll_left.cur=atsyn_move_l mouse_scroll_right.cur=atsyn_move_r 
 ~/lazarus/tools/lazres nicescroll2.res mouse_scroll.png=atsyn_scrollmark mouse_scroll_150.png=atsyn_scrollmark_150 mouse_scroll_200.png=atsyn_scrollmark_200
 
-~/lazarus/tools/lazres editor_hourglass.res editor_hourglass.png=atsyn_wait
-~/lazarus/tools/lazres editor_save.res editor_save.png=atsyn_save
+~/lazarus/tools/lazres editor_hourglass.res editor_hourglass.png=atsyn_wait editor_save.png=atsyn_save
 
 ~/lazarus/tools/lazres foldbar.res fold_plus.png=atsyn_fold_p fold_plus_150.png=atsyn_fold_p_150 fold_plus_200.png=atsyn_fold_p_200 fold_minus.png=atsyn_fold_m fold_minus_150.png=atsyn_fold_m_150 fold_minus_200.png=atsyn_fold_m_200 
