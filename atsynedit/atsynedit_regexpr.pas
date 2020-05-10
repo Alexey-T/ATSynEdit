27493e24f22156fe32b4f3645dcad646607e0134
Alexey <support@uvviewsoft.com>
2020-05-11 00:48:54 +0300

    sync TRegExpr with upstream

--- atsynedit/atsynedit_regexpr.pas
+++ atsynedit/atsynedit_regexpr.pas
@@ -103,8 +103,9 @@ uses
 
 type
   {$IFNDEF FPC}
-  PtrInt = integer;
-  PtrUInt = cardinal;
+  // Delphi doesn't have PtrInt but has NativeInt
+  PtrInt = NativeInt;
+  PtrUInt = NativeInt;
   {$ENDIF}
   {$IFDEF UniCode}
   PRegExprChar = PWideChar;
@@ -769,7 +770,7 @@ uses
 const
   // TRegExpr.VersionMajor/Minor return values of these constants:
   REVersionMajor = 0;
-  REVersionMinor = 987;
+  REVersionMinor = 990;
 
   OpKind_End = REChar(1);
   OpKind_MetaClass = REChar(2);
@@ -3455,7 +3456,8 @@ function TRegExpr.regrepeat(p: PRegExprChar; AMax: integer): integer;
 var
   scan: PRegExprChar;
   opnd: PRegExprChar;
-  TheMax, NLen: integer;
+  TheMax: PtrInt; // PtrInt, gets diff of 2 pointers
+  //NLen: integer;
   InvChar: REChar; // ###0.931
   GrpStart, GrpEnd: PRegExprChar; // ###0.936
   ArrayIndex: integer;
@@ -3476,9 +3478,12 @@ begin
       end;
     OP_EXACTLY:
       begin // in opnd can be only ONE char !!!
+        {
+        // Alexey: commented because of https://github.com/andgineer/TRegExpr/issues/145
         NLen := PLongInt(opnd)^;
         if TheMax > NLen then
           TheMax := NLen;
+        }
         Inc(opnd, RENumberSz);
         while (Result < TheMax) and (opnd^ = scan^) do
         begin
@@ -3488,9 +3493,12 @@ begin
       end;
     OP_EXACTLYCI:
       begin // in opnd can be only ONE char !!!
+        {
+        // Alexey: commented because of https://github.com/andgineer/TRegExpr/issues/145
         NLen := PLongInt(opnd)^;
         if TheMax > NLen then
           TheMax := NLen;
+        }
         Inc(opnd, RENumberSz);
         while (Result < TheMax) and (opnd^ = scan^) do
         begin // prevent unneeded InvertCase //###0.931
