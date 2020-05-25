edeec425e7326c9c61d26087ef8e2168ba90b4fb
Alexey <support@uvviewsoft.com>
2020-05-25 17:07:51 +0300

    now use LineIndexer more

--- atsynedit/atsynedit.pas
+++ atsynedit/atsynedit.pas
@@ -6724,7 +6724,7 @@ begin
     if Carets.Count>0 then
     begin
       i:= Carets[0].PosY;
-      nRange:= FFold.FindDeepestRangeContainingLine(i, Indexes);
+      nRange:= FFold.FindDeepestRangeContainingLine2(i);
     end;
 
   NColorNormal:= Colors.BlockStaple;
