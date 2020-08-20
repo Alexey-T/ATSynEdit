7f1f2810067705b78d5ec3c33a9ad75bc4b491fc
Alexey <support@uvviewsoft.com>
2020-08-21 00:43:13 +0300

    upd converter script

--- doc/adapt_tregexpr.py
+++ doc/adapt_tregexpr.py
@@ -5,13 +5,14 @@
 import sys
 import os
 
-def rep(l, s1, s2):
+def rep(l, s1, s2, needed=True):
     for (i, s) in enumerate(l):
         if s==s1:
             l[i]=s2
             return
-    print('Cannot find required line: "'+s1+'"')
-    sys.exit()
+    if needed:
+        print('Cannot find required line: "'+s1+'"')
+        sys.exit()
 
 fn = os.path.join(os.path.dirname(__file__), 'regexpr.pas')
 fn2 = os.path.join(os.path.dirname(__file__), 'atsynedit_regexpr.pas')
@@ -32,7 +33,8 @@ rep(l, '  regexpr_unicodedata;',
 rep(l, '{$I regexpr_compilers.inc}', 
        '//{$I regexpr_compilers.inc}')
 rep(l, '{ off $DEFINE UniCode} // Use WideChar for characters and UnicodeString/WideString for strings',
-       '{$DEFINE UniCode}')
+       '{$DEFINE UniCode}',
+       needed=False)
 rep(l, '  RegExprModifierS: boolean = True; // default value for ModifierS',
        '  RegExprModifierS: boolean = False; // default value for ModifierS')
 rep(l, '  RegExprModifierM: boolean = False; // default value for ModifierM',
