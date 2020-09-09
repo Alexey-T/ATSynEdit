74b0c1ac705941ea63b78a9b6f0413bcef7a8b27
Alexey <support@uvviewsoft.com>
2020-09-09 13:21:41 +0300

    atsynedit_gdeque no more needed

--- atsynedit/atsynedit_package.pas
+++ atsynedit/atsynedit_package.pas
@@ -18,7 +18,7 @@ uses
   ATSynEdit_Adapter_Cache, ATSynEdit_Gaps, ATSynEdit_DimRanges, 
   ATSynEdit_Bookmarks, ATSynEdit_Gutter_Decor, ATSynEdit_fgl, 
   ATSynEdit_Micromap, atsynedit_unicodedata, ATSynEdit_LinkCache, 
-  ATSynEdit_CharSizeArray, ATSynEdit_LineParts, atsynedit_gdeque, 
+  ATSynEdit_CharSizeArray, ATSynEdit_LineParts, 
   LazarusPackageIntf;
 
 implementation
