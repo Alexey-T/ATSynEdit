1842796eb6f5e34c5a313a70d11bc3e3cde564b8
Alexey <support@uvviewsoft.com>
2021-02-27 13:03:51 +0300

    public CaretsSel obj

--- atsynedit/atsynedit.pas
+++ atsynedit/atsynedit.pas
@@ -1284,6 +1284,7 @@ type
     property Strings: TATStrings read GetStrings write SetStrings;
     property Fold: TATSynRanges read FFold;
     property Carets: TATCarets read FCarets;
+    property CaretsSel: TATCaretSelections read FSel; //not used by apps (at 2021.02), but let's publish, to not forget
     property Markers: TATMarkers read GetMarkers;
     property Attribs: TATMarkers read GetAttribs;
     property Micromap: TATMicromap read FMicromap;
