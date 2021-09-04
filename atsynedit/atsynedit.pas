e8ff1e1e7d5c777243e9b383495f12c70f4cdfcd
Alexey <support@uvviewsoft.com>
2021-09-04 10:51:15 +0300

    fix regex URL, fix https://github.com/Alexey-T/CudaText/issues/3695

--- atsynedit/atsynedit.pas
+++ atsynedit/atsynedit.pas
@@ -473,7 +473,7 @@ const
   cUrlRegex_Email = '\b(mailto:)?\w[\w\-\+\.]*@\w[\w\-\.]*\.\w{2,}\b';
   cUrlRegex_WebBegin = 'https?://|ftp://|magnet:\?|www\.|ftp\.';
   cUrlRegex_WebSite = '\w[\w\-\.@]*(:\d+)?'; // @ for password; :\d+ is port
-  cUrlRegex_WebAnchor = '(\#[\w\-]*)?';
+  cUrlRegex_WebAnchor = '(\#[\w\-/]*)?';
   cUrlRegex_WebParams = '(\?[^<>''"\s]+)?';
   cUrlRegex_Web =
     '\b(' + cUrlRegex_WebBegin + ')'
