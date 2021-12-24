cb2f307e9639aa8266c306ffe71a727c29e3eacb
Alexey <support@uvviewsoft.com>
2021-12-24 22:37:07 +0300

    fix width of ellipsis char

--- atsynedit/atsynedit_charsizearray.pas
+++ atsynedit/atsynedit_charsizearray.pas
@@ -135,13 +135,14 @@ begin
 
   for i:= $2010 to $2027 do
     FixedSizes[i]:= uw_normal;
+  FixedSizes[$2026]:= 0; //ellipsis char should be full-width? but user wants normal width, CudaText issue #3873
+
   for i:= $2030 to $203A do
     FixedSizes[i]:= uw_normal;
   //"Superscripts and Subscripts" block which goes from 0x2070 to 0x209F
   for i:= $2070 to $209F do
     FixedSizes[i]:= uw_normal;
 
-  FixedSizes[$2026]:= uw_fullwidth; //ellipsis char should be full-width
   FixedSizes[$20AC]:= uw_normal;
   FixedSizes[$2122]:= uw_normal;
 
