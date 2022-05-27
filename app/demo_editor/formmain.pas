72762ff964dcf823ac3aa7626f56daa851c8c202
Alexey <support@uvviewsoft.com>
2022-05-27 14:57:36 +0300

    fix demo

--- app/demo_editor/formmain.pas
+++ app/demo_editor/formmain.pas
@@ -1,7 +1,6 @@
 unit formmain;
 
 {$mode objfpc}{$H+}
-{$define test_pro_font}
 
 interface
 
@@ -309,11 +308,6 @@ begin
   ed.Font.Name:= 'Courier New';
   if Screen.Fonts.IndexOf(cGoodFont)>=0 then
     ed.Font.Name:= cGoodFont;
-  {$endif}
-
-  {$ifdef test_pro_font}
-  ed.Font.Name:= 'DejaVu Serif';
-  ATEditorOptions.FontProportional:= true;
   {$endif}
 
   SetLength(ed.Micromap.Columns, 0);
