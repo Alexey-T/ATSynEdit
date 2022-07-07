881d89d45fb202c754a0d8180193198e5f220eb5
Alexey <support@uvviewsoft.com>
2022-07-07 17:06:48 +0300

    fix in TATMarkers.Add

--- atsynedit/atsynedit_markers.pas
+++ atsynedit/atsynedit_markers.pas
@@ -327,6 +327,8 @@ begin
       begin
         while IsIndexValid(NIndex) and (Items[NIndex]=Item) do
           FList.Delete(NIndex);
+        while IsIndexValid(NIndex-1) and (Items[NIndex-1]=Item) do
+          FList.Delete(NIndex-1);
       end
       else
       repeat
