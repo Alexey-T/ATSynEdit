6a8960f0d1469af54972dcac6270f4395c516456
Alexey <support@uvviewsoft.com>
2022-07-17 09:16:39 +0300

    optim Bookmarks.DeleteByTag

--- atsynedit/atsynedit_bookmarks.pas
+++ atsynedit/atsynedit_bookmarks.pas
@@ -154,15 +154,23 @@ end;
 
 function TATBookmarks.DeleteByTag(const ATag: Int64): boolean;
 var
-  i: integer;
+  i, j: integer;
 begin
   Result:= false;
-  for i:= FList.Count-1 downto 0 do
+  i:= FList.Count;
+  repeat
+    Dec(i);
+    if i<0 then Break;
     if FList[i].Data.Tag=ATag then
     begin
       Result:= true;
-      Delete(i);
+      j:= i;
+      while (j>0) and (FList[j-1].Data.Tag=ATag) do
+        Dec(j);
+      FList.DeleteRange(j, i);
+      i:= j;
     end;
+  until false;
 end;
 
 function TATBookmarks.Count: integer; inline;
