79a3c8d8a2f1fa9956121d1e251941b49d99f64c
Alexey <support@uvviewsoft.com>
2022-11-05 20:55:23 +0300

    add helper unused prop

--- atsynedit/atsynedit.pas
+++ atsynedit/atsynedit.pas
@@ -1468,6 +1468,7 @@ type
     IsModifiedGutterNumbersVisible: boolean;
     IsModifiedGutterFoldingVisible: boolean;
     IsModifiedGutterBookmarksVisible: boolean;
+    IsModifiedGutterLineStatesVisible: boolean;
     IsModifiedUnprintedVisible: boolean;
     IsModifiedUnprintedSpaces: boolean;
     IsModifiedUnprintedTrailingOnly: boolean;
