613bb3b294d9796a13e836bcc6101cdb2f4dd349
Alexey <support@uvviewsoft.com>
2023-11-13 08:44:35 +0300

    url regex: fix https://github.com/Alexey-T/CudaText/issues/5269

--- atsynedit/atsynedit.pas
+++ atsynedit/atsynedit.pas
@@ -534,7 +534,7 @@ type
     cUrlRegex_Web =
       '\b(' + cUrlRegex_WebBegin + ')'
       + cUrlRegex_WebSite
-      + '(/[~\w\.\-\+\/%@!%\#]*)?' //folders
+      + '(/[~\w\.\-\+\/%@!%\#\(\)]*)?' //folders
       + cUrlRegex_WebParams
       + cUrlRegex_WebAnchor;
     cUrlRegexInitial = cUrlRegex_Email + '|' + cUrlRegex_Web;
