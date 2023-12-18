b7bc54c369fb7a30be963f20481e9ce3813546d1
Alexey <support@uvviewsoft.com>
2023-12-18 19:34:27 +0300

    PosDetails record: add field

--- atsynedit/atsynedit.pas
+++ atsynedit/atsynedit.pas
@@ -155,6 +155,7 @@ type
 
   TATEditorPosDetails = record
     EndOfWrappedLine: boolean; //coordinate was after the end of wrapped line-part (but not after the final line-part)
+    EndOfPartiallyFoldedLine: boolean; //coordinate was after beginning of folded-block, for partially folded line
     BelowAllText: boolean; //coordinate was below all existing lines (so the ending text pos was returned)
     OnGapItem: TATGapItem; //if coordinate is on a gap, here it TATGapItem object
     OnGapPos: TPoint; //if coordinate is on a gap, here are gap-related coordinates
