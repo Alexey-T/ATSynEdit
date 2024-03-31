789fa3351d2efafa04a1c91fcb84e75182f35dd5
Alexey <support@uvviewsoft.com>
2024-03-31 16:12:23 +0300

    fgl: sync Expand with upstream

--- atsynedit/atsynedit_fgl.pas
+++ atsynedit/atsynedit_fgl.pas
@@ -669,18 +669,21 @@ function TFPSList.Expand: TFPSList;
 var
   IncSize : Longint;
 begin
-  if FCount < FCapacity then exit;
-  IncSize := 4;
-  if FCapacity > 3 then IncSize := IncSize + 4;
-  if FCapacity > 8 then IncSize := IncSize + 8;
-  if FCapacity > 127 then Inc(IncSize, FCapacity shr 2);
-  if FCapacity+IncSize>MaxListSize then
-    IncSize:=MaxListSize-FCapacity;
+  Result := Self;
+  if FCount < FCapacity then 
+    exit;
+  if FCapacity > 127 then 
+    IncSize:=FCapacity shr 2
+  else if FCapacity > 8 then 
+    IncSize := 16
+  else if FCapacity > 3 then 
+    IncSize := 8
+  else
+    IncSize := 4;  
   // If we were at max capacity already, force error.
   If IncSize<=0 then
     IncSize:=1; // Will trigger error 
   SetCapacity(FCapacity + IncSize);
-  Result := Self;
 end;
 
 function TFPSList.GetFirst: Pointer;
