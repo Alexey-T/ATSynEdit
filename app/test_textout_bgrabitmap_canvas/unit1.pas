b1df6cd6ae2c0674ab8218fc352cbaa748fd5583
Alexey T. <support@uvviewsoft.com>
2025-12-25 19:51:58 +0300

    add bgrabitmap-test

---
+++ app/test_textout_bgrabitmap_canvas/unit1.pas
@@ -0,0 +1,86 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs,
+  BGRABitmap, BGRABitmapTypes, BGRAText, BGRAPath;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    procedure FormCreate(Sender: TObject);
+    procedure FormMouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer);
+    procedure FormPaint(Sender: TObject);
+  private
+
+  public
+    mx,my: integer;
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.FormMouseMove(Sender: TObject; Shift: TShiftState; X,
+  Y: Integer);
+begin
+  mx := x;
+  my := y;
+  Invalidate;
+end;
+
+procedure TForm1.FormPaint(Sender: TObject);
+const
+  zoom = 1;
+  cnt = 2000;
+var
+  tick_bgra, tick_cnv: QWord;
+  image: TBGRABitmap;
+  i: integer;
+begin
+  image := TBGRABitmap.Create(ClientWidth div zoom, ClientHeight div zoom, BGRAWhite);
+  image.FontName := Font.Name;
+  image.FontHeight := round(Font.Height*96/PixelsPerInch);
+  image.FontQuality := fqSystemClearType;
+
+  tick_bgra:= GetTickCount64;
+  for i:= 0 to cnt do
+    image.TextOut(240,190, 'TextOut LTR TextOut LTR TextOut LTR TextOut LTR TextOut LTR', BGRABlack);
+  tick_bgra:= GetTickCount64-tick_bgra;
+
+  image.Draw(Canvas, 0,0, true);
+  image.Free;
+
+  //-----------------
+
+  Canvas.Brush.Style := bsClear;
+  Canvas.Font.Height := round(Font.Height*96/PixelsPerInch);
+  Canvas.Font.Color := clBlack;
+
+  tick_cnv:= GetTickCount64;
+  for i:= 0 to cnt do
+    Canvas.TextOut(250,140, 'TextOut LTR TextOut LTR TextOut LTR TextOut LTR TextOut LTR');
+  tick_cnv:= GetTickCount64-tick_cnv;
+
+  //-----------------
+
+  Canvas.TextOut(10, 10, 'time of TextOut: bgrabitmap/tcanvas = '+FloatToStr(tick_bgra/tick_cnv));
+end;
+
+end.
+
