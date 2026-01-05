45655cf7f31534bf8b1dfb03af0202734ab06d5c
Alexey T. <support@uvviewsoft.com>
2026-01-05 20:14:59 +0300

    rename global func

--- atsynedit/atsynedit.pas
+++ atsynedit/atsynedit.pas
@@ -2338,7 +2338,7 @@ uses
 {$endif}
 
 
-function GapsSize(
+function _GapsSize(
   AStrings: TATStrings;
   AGaps: TATGaps;
   AEditorIndex: integer;
@@ -3099,10 +3099,10 @@ begin
       NPos:= Max(0, FScrollVert.NPos);
       if FWrapInfo.IsIndexValid(NPos) then
         NLineIndex:= FWrapInfo.Data[NPos].NLineIndex;
-      NGapPos:= GapsSize(Strings, Gaps, EditorIndex, -1, NLineIndex-1, false);
+      NGapPos:= _GapsSize(Strings, Gaps, EditorIndex, -1, NLineIndex-1, false);
     end;
 
-    NGapAll:= GapsSize(Strings, Gaps, EditorIndex, 0, 0, true);
+    NGapAll:= _GapsSize(Strings, Gaps, EditorIndex, 0, 0, true);
   end;
 
   if not ModeOneLine then
@@ -6775,7 +6775,7 @@ begin
     repeat
       NLineIndex:= AWrapInfo.Data[NPos].NLineIndex - 1;
       NPixels:= APos - NPos* AInfo.CharSizeScaled div ATEditorCharXScale
-        - GapsSize(AStrings, AGaps, AEditorIndex, -1, NLineIndex, false);
+        - _GapsSize(AStrings, AGaps, AEditorIndex, -1, NLineIndex, false);
       if NPos=0 then Break;
       if NLineIndex=0 then Break;
       if NPixels>=0 then Break;
