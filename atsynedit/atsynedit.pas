166d9de72285ce00ec7fe65487bbba652ffa668a
Alexey T. <support@uvviewsoft.com>
2026-01-30 00:15:32 +0300

    change new helper field

--- atsynedit/atsynedit.pas
+++ atsynedit/atsynedit.pas
@@ -506,6 +506,13 @@ type
     DefaultBG
     );
 
+  TATEditorCaretSlowFilter = (
+    None,
+    SelOne,
+    SelMany,
+    SelReset
+    );
+
 type
   { TATSynEdit }
 
@@ -1625,7 +1632,7 @@ type
     Colors: TATEditorColors;
     TagString: string; //to store plugin specific data in CudaText
     ModifiedOptions: TATEditorModifiedOptions;
-    EventCaretSlow_Filter: string; //for CudaText
+    EventCaretSlow_Filter: TATEditorCaretSlowFilter; //for CudaText
     EventCaretSlow_WithSel: boolean; //for CudaText
 
     //overrides
