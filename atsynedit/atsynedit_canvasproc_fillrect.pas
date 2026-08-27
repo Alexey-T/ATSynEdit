9b0c2cba0a4506aedb2d62111b669487bef51751
Alexey T <support@uvviewsoft.com>
2026-08-27 21:16:14 +0300

    optim CanvasFillRect for gtk2

--- atsynedit/atsynedit_canvasproc_fillrect.pas
+++ atsynedit/atsynedit_canvasproc_fillrect.pas
@@ -16,11 +16,49 @@ procedure CanvasFillRect(C: TCanvas; const R: TRect; AColor: TColor);
 implementation
 
 uses
+  {$ifdef LCLGtk2}
+  Gdk2,
+  Gtk2Def,
+  {$endif}
   {$ifdef LCLGtk3}
   Gtk3Objects,
   Cairo,
   {$endif}
   LCLType;
+
+{$ifdef LCLGtk2}
+{$define HAS_F}
+procedure CanvasFillRect(C: TCanvas; const R: TRect; AColor: TColor);
+var
+  DC: TGtkDeviceContext;
+  drawable: PGdkDrawable;
+  temp_gc: PGdkGC;
+  col: TGdkColor;
+  W, H: Integer;
+begin
+  if (C.Handle = 0) then Exit;
+
+  W := R.Width;
+  H := R.Height;
+  if (W <= 0) or (H <= 0) then Exit;
+
+  DC := TGtkDeviceContext(C.Handle);
+  drawable := DC.Drawable;
+  if drawable = nil then Exit;
+  temp_gc := DC.GC;
+  if temp_gc = nil then Exit;
+
+  // GDK2 uses 16-bit color components
+  // multiply by $101 (257) - to scale 0..255 -> 0..65535
+  col.red   := (AColor and $FF) * $101;
+  col.green := ((AColor shr 8) and $FF) * $101;
+  col.blue  := ((AColor shr 16) and $FF) * $101;
+
+  gdk_gc_set_rgb_fg_color(temp_gc, @col);
+
+  gdk_draw_rectangle(drawable, temp_gc, 1, R.Left, R.Top, W, H);
+end;
+{$endif}
 
 {$ifdef LCLGtk3}
 {$define HAS_F}
