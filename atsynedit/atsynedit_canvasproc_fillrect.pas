50dea44cbbfb142ffa6873ac10e826ae3a2f46a1
Alexey T <support@uvviewsoft.com>
2026-08-27 21:01:04 +0300

    optim CanvasFillRect for gtk3

--- atsynedit/atsynedit_canvasproc_fillrect.pas
+++ atsynedit/atsynedit_canvasproc_fillrect.pas
@@ -15,11 +15,41 @@ procedure CanvasFillRect(C: TCanvas; const R: TRect; AColor: TColor);
 
 implementation
 
+uses
+  {$ifdef LCLGtk3}
+  Gtk3Objects,
+  Cairo,
+  {$endif}
+  LCLType;
+
+{$ifdef LCLGtk3}
+{$define HAS_F}
+procedure CanvasFillRect(C: TCanvas; const R: TRect; AColor: TColor);
+var
+  Cr: Pcairo_t;
+begin
+  if (C.Handle = 0) then Exit;
+
+  cr := pcairo_t(TGtk3DeviceContext(C.Handle).pcr);
+  if cr = nil then Exit;
+
+  cairo_set_source_rgb(Cr,
+    (AColor and $FF) / 255.0,
+    ((AColor shr 8) and $FF) / 255.0,
+    ((AColor shr 16) and $FF) / 255.0);
+
+  cairo_rectangle(Cr, R.Left, R.Top, R.Width, R.Height);
+  cairo_fill(Cr);
+end;
+{$endif}
+
+{$ifndef HAS_F}
 procedure CanvasFillRect(C: TCanvas; const R: TRect; AColor: TColor);
 begin
   C.Brush.Style:= bsSolid;
   C.Brush.Color:= AColor;
   C.FillRect(R);
 end;
+{$endif}
 
 end.
